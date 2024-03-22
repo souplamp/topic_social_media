d4ff63bb30505d0e4cb776ab480a42b058fe575b
souplamp <camden.mcall@gmail.com>
2024-03-21 19:51:30 -0500

    it mostly working

--- testmongo.js
+++ testmongo.js
@@ -17,7 +17,7 @@ routes:
 
 // The uri string must be the connection string for the database (obtained on Atlas).
 
-const uri = "mongodb+srv://<user>:<password>@ckmdb.5oxvqja.mongodb.net/?retryWrites=true&w=majority";
+const uri = "mongodb+srv://<user>:<password>@cluster0.6k7ugaa.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 // --- This is the standard stuff to get it to work on the browser
 const cookieParser = require('cookie-parser');
@@ -48,25 +48,57 @@ app.get('/', function(req, res) {
    content += "Results: " + JSON.stringify(mycookies);
   } else {
     content += "NO RESULTS! <br> Redirecting to <a href='/login'>login page</a>...";
-    content += "<script>setTimeout(function(){window.location='/login';},2000)</script>"
+    content += "<script>setTimeout(function(){window.location='/login';},2000)</script>" // redirect to login
   }
 
   res.send(content);
 
 });
 
-app.get('/login', function(req, res) {
-  content = "<p>Login <a href='https://bingus.com'>here.</a><p>"
-  //res.send(content);
-  // get info from the login.. could just take the query after and if the content is good..
-  // oh baby that's the idea, just take the query string and test the mongodb.
-  // if its in there, move on to the print main page.
-  // see below
-  console.log(req.query);
-  // get the req.query
 
-  res.render("login");
+
+const asyncFunc = () => {
+  return new Promise((resolve) => {
+    setTimeout(() => resolve("Hello World!"), 1000)
+  })
+}
+
+app.get('/', async (req, res) => {
+  const result = await asyncFunc()
+  return res.send(result)
+})
+
+const query_database = (query) => {
+  content = "";
+  const client = new MongoClient(uri);
+  async function run() {
+    try {
+      const database = client.db('Database');
+      const users = database.collection('MyStuff');
+      const q = { username: query.username, password: query.password };
+      const result = await users.findOne(q);
+      content = result;
+      console.log(content);
+    } finally {
+      await client.close();
+      return new Promise((resolve) => {
+        setTimeout(() => resolve(content), 1000);
+      })
+    }
+  }
+  run().catch(console.dir);
+}
+
+app.get('/login', async (req, res) => {
+  //stringified_query = JSON.stringify(req.query);
+  content = await query_database(req.query);
+  res.render("login", { bigchunkus: content });
 });
+
+  // run try catch block, if in database
+
+  //res.render("login");
+//});
 
 /* use this to access teh database
 
